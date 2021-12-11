2187d0eb6ced1150c09c63d8322dbafef16e68ed
Papeleria Cachorros <papeleriacachorros@MacBook-Air-de-Jose.local>
2021-12-10 23:24:50 -0600

    initial commit

---
+++ MAIN.js
@@ -0,0 +1,47 @@
+
+/*
+"user strict"
+
+let body = document.querySelector("body")
+
+//.createElement( ) me permite crear un elemento
+let tittle = document.createElement("h1")
+
+//Agregar atributos
+tittle.id = "tittle"
+tittle.className = "tittle-class"
+
+tittle.textContent = "ESTE ES UN TITULO DINAMICO" 
+span.textContent = "span"
+
+tittle.appendChild( span )
+
+//inserta un elemento como ultimo hijo de otro elemento
+body.appendChild( tittle )
+
+
+//Inserta un elemento antes de los hijos que ya existen en otro elemento
+let img = document.createElement( "img" )
+img.src = "http://picsum.photos/seed/picsum/200/300"
+
+body.prepend( img )
+
+//Elimina elementos del DOM
+let paragraph = document.getElementsByTagName("p")[0]
+
+paragraph.remove()
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
